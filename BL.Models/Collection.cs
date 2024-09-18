c3323cc8a3b95b851d02b951a8f19babd831adef
Derek Peerenboom <500144684@fvtc.edu>
2024-09-17 20:53:49 -0500

    Basic models in the BL

---
+++ BL.Models/Collection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Models
+{
+    public class Collection
+    {
+        public Guid Id { get; set; }
+        public Guid CourseId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
