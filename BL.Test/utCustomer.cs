17174589aaaf9129b185b3a573488155bbffedd3
Derek Peerenboom <500144684@fvtc.edu>
2024-10-23 20:12:04 -0500

    Add customer test

---
+++ BL.Test/utCustomer.cs
@@ -0,0 +1,56 @@
+﻿using Elevate.BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevate.BL.Test
+{
+    [TestClass]
+    public class utCustomer
+    {
+        //[TestMethod]
+        //public void InsertTest()
+        //{
+        //    Customer customer = new Customer
+        //    {
+        //        FirstName = "Jacob",
+        //        LastName = "Peters",
+        //        Address = "123 Nope Rd",
+        //        City = "Grecko",
+        //        State = "WI",
+        //        Zip = "24234",
+        //        Phone = "1234567898",
+        //        UserId = 1234
+        //    };
+
+        //    Assert.AreEqual(1, CustomerManager.Insert(customer, true));
+        //}
+
+        //[TestMethod()]
+        //public void LoadTest()
+        //{
+        //    Assert.AreEqual(3, CustomerManager.Load().Count);
+        //}
+
+        //[TestMethod()]
+        //public void LoadByIdTest()
+        //{
+        //    Assert.AreEqual(1, CustomerManager.LoadById(1).Id);
+        //}
+
+        //[TestMethod]
+        //public void UpdateTest()
+        //{
+        //    Customer customer = CustomerManager.LoadById(3);
+        //    customer.FirstName = "Andrew";
+        //    Assert.IsTrue(CustomerManager.Update(customer, true) > 0);
+        //}
+        //[TestMethod]
+        //public void DeleteTest()
+        //{
+        //    Assert.AreEqual(1, CustomerManager.Delete(2, true));
+        //}
+    }
+}
