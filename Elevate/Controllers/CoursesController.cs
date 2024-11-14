cde3d88d2234f9eba6dfed3a41b2ddde9d56d767
Dawson Knudtson <300078204@fvtc.edu>
2024-11-13 20:23:41 -0600

    some changes for courses page, broke site lol

---
+++ Elevate/Controllers/CoursesController.cs
@@ -0,0 +1,32 @@
+﻿using Elevate.PL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elevate.UI.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ILogger<CoursesController> _logger;
+        private readonly ElevateEntities _context;
+
+        // Combined constructor
+        public CoursesController(ILogger<CoursesController> logger, ElevateEntities context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var tblCourses = _context.tblCourses.ToList();
+
+            var courses = tblCourses.Select(c => new Elevate.BL.Models.Course
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Description = c.Description,
+                Cost = c.Cost
+            }).ToList();
+
+            return View(courses);
+        }
+    }
+}
