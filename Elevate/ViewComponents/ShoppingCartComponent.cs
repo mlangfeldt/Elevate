df09fa92e415717b3aa42175dc5f90e2398ad883
Zachary Sullivan <300083331@fvtc.edu>
2024-11-18 17:22:01 -0600

    Shopping Cart item counter functionality working.

--- Elevate/ViewComponents/ShoppingCartComponent.cs
+++ Elevate/ViewComponents/ShoppingCartComponent.cs
@@ -6,16 +6,20 @@ namespace Elevate.UI.ViewComponents
 {
     public class ShoppingCartComponent : ViewComponent
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ShoppingCartComponent(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         public IViewComponentResult Invoke()
         {
-            if (HttpContext.Session.GetObject<ShoppingCart>("cart") != null)
-            {
-                return View(HttpContext.Session.GetObject<ShoppingCart>("cart"));
-            }
-            else
-            {
-                return View(new ShoppingCart());
-            }
+            // Retrieve the ShoppingCart from the session, or create a new one if it doesn't exist
+            var cart = _httpContextAccessor.HttpContext.Session.GetObject<ShoppingCart>("cart") ?? new ShoppingCart();
+
+            // Pass the cart to the view
+            return View(cart);
         }
     }
 }
