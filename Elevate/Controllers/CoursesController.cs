72fe9a6783d434ab05fd6f54d0031d4619ab1d8c
Monica Langfeldt <300087056@fvtc.edu>
2024-11-21 12:46:07 -0600

    Reviews added again

--- Elevate/Controllers/CoursesController.cs
+++ Elevate/Controllers/CoursesController.cs
@@ -49,8 +49,35 @@ namespace Elevate.UI.Controllers
                 Cost = courseEntity.Cost
             };
 
+            // Hard-code reviews based on the courseId
+            List<dynamic> reviews = new List<dynamic>();
+
+            if (id == 1) // Retirement
+            {
+                reviews.Add(new { Rating = 5, ReviewText = "This course helped me a lot in planning my future. Highly recommend!", ReviewerName = "Kelly Rowan" });
+                reviews.Add(new { Rating = 4, ReviewText = "Great course with useful tips, but could use more examples.", ReviewerName = "Jeff Dunham" });
+            }
+            else if (id == 2) // Debt Management
+            {
+                reviews.Add(new { Rating = 4, ReviewText = "Informative, but I would have liked more detailed steps on budgeting.", ReviewerName = "Java Script" });
+                reviews.Add(new { Rating = 5, ReviewText = "A must-take course for anyone struggling with debt. Fantastic!", ReviewerName = "Joe Smith" });
+            }
+            else if (id == 3) // Investment
+            {
+                reviews.Add(new { Rating = 5, ReviewText = "This course was excellent. It provided me with all the tools I need to start investing.", ReviewerName = "Lisa Simon" });
+                reviews.Add(new { Rating = 3, ReviewText = "Good course, but the content was a bit basic for my level.", ReviewerName = "James Brown" });
+            }
+            else if (id == 4) // Budgeting
+            {
+                reviews.Add(new { Rating = 5, ReviewText = "I learned so much from this course, and it's helped me stick to a budget.", ReviewerName = "Mike White" });
+                reviews.Add(new { Rating = 4, ReviewText = "Great introduction to budgeting, but I wish there were more advanced topics.", ReviewerName = "Sarah Adams" });
+            }
+
+            ViewData["Reviews"] = reviews;
+
             return View(course);
         }
+
 
     }
 }
