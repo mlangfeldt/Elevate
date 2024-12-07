d26edc4b425a24054016f1e171ac51a2cc4f8bfb
Derek Peerenboom <500144684@fvtc.edu>
2024-12-07 13:37:13 -0600

    Playing with search bar. Added courses in default data. Did not update database.

--- Elevate.DB/PostDeploy/DefaultData/Courses.sql
+++ Elevate.DB/PostDeploy/DefaultData/Courses.sql
@@ -4,5 +4,9 @@
 	(1,'Budget','This course teaches you the ins and outs of creating a budget.', 15.99, 'budget.jpg'),
 	(2,'Debt Management','This course teaches you how to handle the debt you have.', 12.99, 'debt.jpg'),
 	(3,'Investment','This course teaches you how to start investing in the stock market.', 18.99, 'invest.jpg'),
-	(4,'Retirement','This course teaches you how to manage retirement.', 25.99, 'retire.jpg')
+	(4,'Retirement','This course teaches you how to manage retirement.', 25.99, 'retire.jpg'),
+	(5,'Cryptocurrency','This course teaches you foundational crypto knowledge.', 21.99, 'crypto.jpg'),
+	(6,'Debt Leverage','This course teaches you how to leverage the debt you have.', 26.99, 'debtleverage.jpg'),
+	(7,'Investment Diversification','This course teaches you how to diversify your investments in the stock market.', 29.99, 'investdiversify.jpg'),
+	(8,'Retirement Income','This course teaches you how to manage your retirement income.', 31.99, 'retireplan.jpg')
 END
