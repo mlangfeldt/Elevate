991a15729acf48fdb24864af1c1ad7329144f1cd
Dawson Knudtson <300078204@fvtc.edu>
2024-12-14 20:34:23 -0600

    fixed couple of things

--- Elevate.DB/PostDeploy/DefaultData/Courses.sql
+++ Elevate.DB/PostDeploy/DefaultData/Courses.sql
@@ -5,8 +5,10 @@
 	(2,'Debt Management','This course teaches you how to handle the debt you have.', 12.99, 'debt.jpg'),
 	(3,'Investment','This course teaches you how to start investing in the stock market.', 18.99, 'invest.jpg'),
 	(4,'Retirement','This course teaches you how to manage retirement.', 25.99, 'retire.jpg'),
-	(5,'Cryptocurrency','This course teaches you foundational crypto knowledge.', 21.99, 'crypto.jpg'),
-	(6,'Debt Leverage','This course teaches you how to leverage the debt you have.', 26.99, 'debtleverage.jpg'),
-	(7,'Investment Diversification','This course teaches you how to diversify your investments in the stock market.', 29.99, 'investdiversify.jpg'),
-	(8,'Retirement Income','This course teaches you how to manage your retirement income.', 31.99, 'retireplan.jpg')
+	(5,'Cryptocurrency','This course teaches you foundational crypto knowledge.', 21.99, 'retire.jpg'),
+	(6,'Debt Leverage','This course teaches you how to leverage the debt you have.', 26.99, 'invest.jpg'),
+	(7,'Investment Diversification','This course teaches you how to diversify your investments in the stock market.', 29.99, 'debt.jpg'),
+	(8,'Retirement Income','This course teaches you how to manage your retirement income.', 31.99, 'budget.jpg'),
+	(9,'FinancialLiteracy','Teach teens the fundamentals of financial management to set them up for success', 29.99, 'FinancialLit.jpg'),
+	(10,'CreditLoans','Master the art of credit building and loan management to enhance your financial future.', 31.99, 'CreditLoan.jpg')
 END
